e4a5e46cae9c6be1c16560507d0ffab9052deb93
GaoBin <bin_gao@live.cn>
2014-12-30 16:35:58 +0800

    update all.css report.js TestPJS.java and add phantomjs2.js files

---
+++ src/main/resources/phantomjs2.js
@@ -0,0 +1,82 @@
+var page = require('webpage').create();
+var system = require('system');
+var url = system.args[1];
+var uri = system.args[2];
+var imageName = system.args[3];
+var json = system.args[4];
+var reportType = system.args[5];
+var reportName = system.args[6];
+var pdfName = system.args[7];
+var conversionCurrency = system.args[8];
+var conversionEnabled = system.args[9];
+
+ phantom.onError = function(msg, trace) {
+ 	var msgStack = ['PHANTOM ERROR: ' + msg];
+ 	if (trace && trace.length) {
+ 		msgStack.push('TRACE:');
+ 		trace.forEach(function(t) {
+ 			msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function +')' : ''));
+ 		});
+ 	}
+ 	console.error(msgStack.join('\n'));
+ 	phantom.exit(1);
+ };
+
+
+ page.viewportSize = {
+ 	width : 1300,
+ 	height : 900
+ };
+
+ var fs = require('fs');
+ var content = fs.read(json);
+ var obj = JSON.parse(content);
+
+ page.open(url + uri, function() {	
+ 	var title = "";
+ 	if (reportType == "BATCH") {
+ 		title = ("Batch Mailing Report: " + reportName);
+ 	}
+ 	if (reportType == "TRANSACTIONAL") {
+ 		title = ("Transactional Mailing Report: " + reportName);
+ 	}
+ 	if (reportType == "PROGRAM") {
+ 		title = ("Lifecycle Program Report: " + reportName);
+ 	}
+ 	obj.title = title;
+ 	obj.reportType = reportType;
+ 	obj.conversionEnabled = (typeof conversionCurrency !== "undefined" && conversionCurrency !== null) ? conversionEnabled : "true";
+ 	obj.conversionCurrency =  (typeof conversionCurrency !== "undefined" && conversionCurrency !== null) ? conversionCurrency : "$";
+ 	page.includeJs(url + '/js/report.js', function() {
+ 		doReport(page, obj);
+ 	});
+ 	window.setTimeout(function() {
+ 		page.render(imageName,{format: 'png', quality: '70'});
+ 		if (typeof pdfName !== "undefined" && pdfName !== null) {
+ 			page.evaluate(function () {
+ 				var paths = document.getElementsByTagName("path");
+ 				for (var i = paths.length - 1; i >= 0; i--) {
+ 					var path = paths[i];
+ 					var strokeOpacity = path.getAttribute('stroke-opacity');
+ 					if (strokeOpacity != null && strokeOpacity < 0.2){
+ 						path.parentNode.removeChild(path);
+ 					}
+ 				}
+ 			});
+ 			page.paperSize={format: 'A4',orientation: 'landscape',margin: '1cm'};
+ 			page.render(pdfName,{format: 'pdf', quality: '100'});
+ 		}
+ 		console.log("SUCCESS");
+ 		phantom.exit();
+ 	}, 6000);
+
+ });
+
+function doReport(page, json) {
+	page.evaluate(function(json) {
+		showSummaryChartPart('day', json.items[0].data, json.reportType);
+		showBottomSummaryPart('day', json.items[0].summary, json.reportType, json.conversionCurrency,json.conversionEnabled);
+		$("#reportTitle").html(json.title);
+	}, json);
+}
+
--- src/test/java/TestPJS.java
+++ src/test/java/TestPJS.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 public class TestPJS {
     public String exportChart(String phantomjsPath, String pjsPath, String url, String uri, String imageSavePath,
-                            String jsonPath, String reportType, String reportName, boolean isPDF) {
+                            String jsonPath, String reportType, String reportName, String pdfSavePath, String conversionCurrency,
+                            boolean conversionEnabled) {
         List<String> paramList = new LinkedList<String>();
         paramList.add(phantomjsPath);
         paramList.add(pjsPath);
@@ -15,7 +16,9 @@ public class TestPJS {
         paramList.add(jsonPath);// 4
         paramList.add(reportType);// 5
         paramList.add(reportName);// 6
-        paramList.add(isPDF + "");// 6
+        paramList.add(pdfSavePath);// 7
+        paramList.add(conversionCurrency);// 8
+        paramList.add(conversionEnabled+"");// 9
 
         StringBuilder stringBuilder = null;
         String[] parameters = paramList.toArray(new String[paramList.size()]);
@@ -48,38 +51,45 @@ public class TestPJS {
     // Batch : Batch Mailing Report : <Report Name>
     public static void main(String[] args) {
         try {
-            boolean isPDF = false;
             // for luo
-            String path = "/Users/luo/WORK";
-            String projectPath = "/workspace_snow/luo/phantomjsHighchart/phantomjsHighchart";
-            String phantomjsPath = path + "/tool/phantomjs-1.9.8-macosx/bin/phantomjs";
-            String dataPath = path + projectPath + "/src/main/resources/2";
-            String visitAddress = "http://localhost:8080/phantomjs-highchart";
+//            String path = "/Users/luo/WORK";
+//            String projectPath = "/workspace_snow/luo/phantomjsHighchart/phantomjsHighchart";
+//            String phantomjsPath = path + "/tool/phantomjs-1.9.8-macosx/bin/phantomjs";
+//            String dataPath = path + projectPath + "/src/main/resources/2";
+//            String visitAddress = "http://localhost:8080/phantomjs-highchart";
             // for gao
-            // String path = "/Users/bin_gao/Documents/workspace-4";
-            // String projectPath = "/phantomjsHighchart";
-            // String phantomjsPath = "/usr/local/bin/phantomjs";
-            // String dataPath = path + projectPath + "/src/main/resources/2";
-            // String visitAddress = "http://localhost:8080/";
-
-            String pjsPath = path + projectPath + "/src/main/resources/pjs.js";
+             String path = "/Users/bin_gao/Documents/workspace-4";
+             String projectPath = "/phantomjsHighchart";
+             String phantomjsPath = "/usr/local/bin/phantomjs";
+             String dataPath = path + projectPath + "/src/main/resources/2";
+             String visitAddress = "http://localhost:8080/";
+             
+            String savePath = path + projectPath + "/src/main/resources";
+            String pjsPath = path + projectPath + "/src/main/resources/phantomjs2.js";
             String visitUri = "/";
             String reportName = "TEST";
-            //
-            String imageSavePath = path + projectPath + "/src/main/resources/0batchsummary.png";
+            
+            //setting
+            String conversionCurrency = "$";
+            boolean conversionEnabled = true;
+            
+            String imageSavePath = savePath + "/1batchsummary.png";
+            String pdfSavePath = savePath+ "/1batchsummary.pdf";
             String jsonPath = dataPath + "/batchsummary.json";
-            String reportType = "BATCH";// BATCH|TRANSACTIONAL|PROGRAM
-            new TestPJS().exportChart(phantomjsPath, pjsPath, visitAddress, visitUri, imageSavePath, jsonPath, reportType, reportName, isPDF);
-            //
-            imageSavePath = path + projectPath + "/src/main/resources/0transactionalsummary.png";
+            String reportType = "BATCH";
+            new TestPJS().exportChart(phantomjsPath, pjsPath, visitAddress, visitUri, imageSavePath, jsonPath, reportType, reportName, pdfSavePath, conversionCurrency, conversionEnabled);
+            
+            imageSavePath = savePath + "/1transactionalsummary.png";
+            pdfSavePath = savePath+ "/1transactionalsummary.pdf";
             jsonPath = dataPath + "/transactionalsummary.json";
-            reportType = "TRANSACTIONAL";// BATCH|TRANSACTIONAL|PROGRAM
-            new TestPJS().exportChart(phantomjsPath, pjsPath, visitAddress, visitUri, imageSavePath, jsonPath, reportType, reportName, isPDF);
-            //
-            imageSavePath = path + projectPath + "/src/main/resources/0progamsummary.png";
+            reportType = "TRANSACTIONAL";
+            new TestPJS().exportChart(phantomjsPath, pjsPath, visitAddress, visitUri, imageSavePath, jsonPath, reportType, reportName, pdfSavePath, conversionCurrency, conversionEnabled);
+            
+            imageSavePath = savePath + "/1progamsummary.png";
+            pdfSavePath = savePath+ "/1progamsummary.pdf";
             jsonPath = dataPath + "/progamsummary.json";
-            reportType = "PROGRAM";// BATCH|TRANSACTIONAL|PROGRAM
-            new TestPJS().exportChart(phantomjsPath, pjsPath, visitAddress, visitUri, imageSavePath, jsonPath, reportType, reportName, isPDF);
+            reportType = "PROGRAM";
+            new TestPJS().exportChart(phantomjsPath, pjsPath, visitAddress, visitUri, imageSavePath, jsonPath, reportType, reportName, pdfSavePath, conversionCurrency, conversionEnabled);
 
         } catch (Exception e) {
             e.printStackTrace();
